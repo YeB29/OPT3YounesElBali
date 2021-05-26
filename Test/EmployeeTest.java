8859f45bc7099b24fe8ad681101f6977a4938a87
YeB29 <74351462+YeB29@users.noreply.github.com>
2021-05-26 15:00:17 +0200

    test employee

--- Test/EmployeeTest.java
+++ Test/EmployeeTest.java
@@ -6,7 +6,7 @@ class EmployeeTest {
 
 
     @Test
-    void getEmployeeIDTest() {
+    void employeeIDTest() {
         Employee werknemer1 = new Employee( 478, "Joost");
         assertFalse(werknemer1.getEmployeeID(7834));
         assertTrue(werknemer1.getEmployeeID(478));
@@ -14,10 +14,9 @@ class EmployeeTest {
     }
 
     @Test
-    void getEmployeeNameTest() {
+    void employeeNameTest() {
         Employee werknemer1 = new Employee( 478, "Joost");
         assertFalse(werknemer1.getEmployeeName("Merel"));
         assertTrue(werknemer1.getEmployeeName("Joost"));
-
     }
 }
